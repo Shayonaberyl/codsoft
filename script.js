9381d2533c006848258569e101c2cf216ecee6bb
Shayonaberyl <160497620+Shayonaberyl@users.noreply.github.com>
2024-02-19 18:41:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,113 +1,38 @@
-const questions = [
-    {
-      question:"Most widely spoken language in the world?",
-      answers: [
-        {text: "French", correct: false},
-        {text: "Mandarin", correct: true},
-        {text: "Hindi", correct: false},
-        {text: "English", correct: false},
-          
-      ],
-    },
-    {
-      question: "What does chat GPT stands for",
-      answers: [
-        {text: "Generative Pre-trained Transformer", correct: true},
-        {text: "Global Pre-trained Transformer ", correct: false},
-        {text: "Generative pre-trained Tool", correct: false},
-        {text: "Generative Plug-in Tool", correct: false},
-      ],
-    },
-    {
-      question: "Which of these app is not a meta app?",
-      answers: [
-        {text: "Instragram", correct:false},
-        {text: "Whatsapp", correct: false},
-        {text: "Facebook", correct: false},
-        {text: "Tik-Tok", correct: true},
-      ],
-    },
-    {
-      question: "What programming language is used in Google?",
-      answers: [
-        {text: "C", correct: false},
-        {text: "Java", correct: true},
-        {text: "SQL", correct: false},
-        {text: "Swift", correct: false},
-      ],
-    },
-  ];
-  
-  const questionElement = document.getElementById("question");
-  const answerbutton= document.getElementById("answer-buttons");
-  const nexButton = document.getElementById("next-btn");
-  
-  let currentQuestionIndex = 0;
-  let score = 0;
-  
-  function startQuiz(){
-      currentQuestionIndex = 0;
-      score = 0;
-      nexButton.innerHTML = "Next";
-      showQuestion();
-  
-  }
-  
-  function showQuestion(){
-      resetState();
-      let currentQuestion = questions[currentQuestionIndex];
-      let questionNo = currentQuestionIndex + 1;
-      questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
-  
-      currentQuestion.answers.forEach(answer => {
-         const button = document.createElement("button");
-         button.innerHTML = answer.text;
-         button.classList.add("btn");
-         answerbutton.appendChild(button);
-         if(answer.correct){
-          button.dataset.correct = answer.correct;
-         }
-         button.addEventListener("click",selectAnswer);
-    });
-  }
-  
-  function resetState(){
-     nexButton.style.display = "none";
-     while(answerbutton.firstChild){
-      answerbutton.removeChild(answerbutton.firstChild);
-     
-  }
-  }
-  
-  function selectAnswer(e){
-      const selectedBtn =  e.target;
-      const isCorrect = selectedBtn.dataset.correct === "true";
-      if(isCorrect){
-        selectedBtn.classList.add("correct");
-        score++;
-      }else{
-        selectedBtn.classList.add("incorrect");
-      }
-      Array.from(answerbutton.children).forEach(button => {
-        if(button.dataset.correct === "true"){
-          button.classList.add("correct");
-        }
-        button.disabled = true;
-      });
-      nexButton.style.display = "block";
-      nexButton.addEventListener("click", goToNextQuestion);
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ' '){
+        alert("you must write something!");
     }
-  
-    function goToNextQuestion(){
-      currentQuestionIndex++;
-      if(currentQuestionIndex < questions.length){
-        showQuestion();
-      }else{
-        alert("Quiz finished! Your score: " + score);
-      }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
-  
-  
-  
-  
-  startQuiz();
+    inputBox.value = "";
+    saveData();
+}
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+     localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data)");
+
+}
+showTask();
